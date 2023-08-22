da8ac1a8f08f6d23b848bcc57f8fdc1a5bdd2d1e
João Pedro Perdomo Dassoler <104994947+jpdassoler@users.noreply.github.com>
2023-08-22 15:23:10 -0300

    terminando as aulas com o dev Soutinho

---
+++ 03-minibrowser/set.js
@@ -0,0 +1,13 @@
+// TDD - test driven development
+
+var novoSet = new Set();
+
+novoSet.add('Backend');
+novoSet.add('Frontend');
+novoSet.add('Mobile');
+novoSet.add('Mobile');
+novoSet.add('Mobile');
+novoSet.add('Mobile');
+
+var resultado = novoSet;
+console.log(resultado);
